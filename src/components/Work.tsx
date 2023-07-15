9fbeed5efcdc43fe0278c6538aa05ae1ae243b51
Chris Swan <chrisswan1612@gmail.com>
2023-07-15 22:50:00 +1200

    sort of got the scrolling working

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -3,9 +3,10 @@ function Work() {
     <>
       <section className="section-container__work">
         <div className="section-container__work__gradient"></div>
-        <div className="section-container__work__background" id="work"></div>
-        <div className="section-container__work__content">
-          <h1>Work!</h1>
+        <div className="section-container__work__background" id="work">
+          <div className="section-container__work__content">
+            <h1>Work!</h1>
+          </div>
         </div>
       </section>
     </>
