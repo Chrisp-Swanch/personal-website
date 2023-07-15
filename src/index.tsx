b9c167f896ecd7931a368d7550ac695840e1cd90
Chris Swan <chrisswan1612@gmail.com>
2023-07-15 12:42:57 +1200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+})
